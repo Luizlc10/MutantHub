e61f9b2e823ffee8241df2b5765857d526ff8c77
Luizlc10 <luizclaudiol@hotmail.com>
2018-03-28 08:53:22 -0300

    importing bootstrap/jquery

--- react-src/src/index.js
+++ react-src/src/index.js
@@ -1,6 +1,11 @@
+/*jshint esversion: 6 */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
