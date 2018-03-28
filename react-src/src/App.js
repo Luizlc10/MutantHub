c56678c4fbe1b0fd35c563cff2d9ee0779147540
Luizlc10 <luizclaudiol@hotmail.com>
2018-03-28 12:05:15 -0300

    starting main template and routing

--- react-src/src/App.js
+++ react-src/src/App.js
@@ -5,8 +5,40 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <h1>Mutant Hub</h1>
+      <div className="App container-fluid">
+
+          <header className="row justify-content-center">
+            <div className="row text-light align-self-start">
+              <div className="col-12 text-center">
+                <h1>Mutant Hub</h1>
+              </div>
+            </div>
+          </header>
+
+          <div className="row justify-content-center">
+
+            <div className="navigation-bar">
+                <ul>
+
+                  <li className="nav-item active shadow-1">
+                    <a className="nav-link bg-warning btn" data-toggle="tooltip" data-placement="left" title="Search" href="">
+                      <i className="material-icons">search</i>
+                    </a>
+                  </li>
+                  <li className="nav-item active shadow-1">
+                    <a className="nav-link bg-warning btn" data-toggle="tooltip" data-placement="left" title="Graphics" href="">
+                      <i className="material-icons">data_usage</i>
+                    </a>
+                  </li>
+
+                </ul>
+            </div>
+
+            <div className="col-12 col-sm-10  bg-light main-card shadow-1">
+              <h1>Router</h1>
+            </div>
+          </div>
+
       </div>
     );
   }
